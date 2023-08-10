65079a63933b915fe5096893e3741a8fe8ab58c5
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-10 17:10:47 +0700

    init backend

---
+++ server/src/main/java/com/example/server/configs/SpringSecurityConfig.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.server.configs;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import java.text.SimpleDateFormat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ *
+ * @author admin
+ */
+@Configuration
+@EnableWebSecurity
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+        "com.example.server.controllers",
+        "com.example.server.repositories",
+        "com.example.server.services"
+})
+@PropertySource("classpath:cloudinary.properties")
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private Environment env;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)
+            throws Exception {
+
+        http.formLogin().loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password");
+
+        http.formLogin().defaultSuccessUrl("/")
+                .failureUrl("/login?error");
+
+        http.logout().logoutSuccessUrl("/login");
+        http.exceptionHandling()
+                .accessDeniedPage("/login?accessDenied");
+
+//        http.authorizeRequests().antMatchers("/").permitAll()
+//                .antMatchers("/**/add")
+//                .access("hasRole('ROLE_ADMIN')");
+//        .antMatchers("/**/pay")
+//                .access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
+        http.csrf().disable();
+    }
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Cloudinary cloudinary
+                = new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", this.env.getProperty("cloudinary.cloud_name"),
+                "api_key", this.env.getProperty("cloudinary.api_key"),
+                "api_secret", this.env.getProperty("cloudinary.api_secret"),
+                "secure", true));
+        return cloudinary;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }
+}
