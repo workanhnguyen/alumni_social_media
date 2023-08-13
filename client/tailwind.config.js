5aa3562f9dbd7175dedabef0e6e9e6919f41dc07
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-13 18:11:39 +0700

    add configs

---
+++ client/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        primary: '#1b73e8',
+        primaryHover: '#0fb3fa',
+        secondary: '#F0F0F0',
+        blackOverlay: 'rgba(0, 0 ,0 ,0.7)',
+        halfTransparent: 'rgba(0, 0, 0, 0.5)',
+      },
+      backgroundImage: {
+        'cover-bg': "url('./assets/cover-bg.png')"
+      },
+      blur: {
+        xs: '2px'
+      },
+      colors: {
+        'half-transparent': 'rgb((0, 0, 0) / 0.5)'
+      }
+    },
+  },
+  plugins: [],
+}
