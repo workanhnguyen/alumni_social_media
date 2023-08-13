104a1c930dffcd7db0e3ad433ddda733dbe47391
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-13 18:13:25 +0700

    add index file of pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as ChooseRole } from './ChooseRole';
+export { default as AdminLogin } from './admin/AdminLogin';
+export { default as AlumniLogin } from './alumni/AlumniLogin';
+export { default as LecturerLogin } from './lecturer/LecturerLogin';
