aa4a6f86905f6aba508b424e9057eeba848af43b
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-17 11:58:51 +0700

    handle change content

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -2,12 +2,19 @@ import React from "react";
 
 import { Container } from "@mui/material";
 import { Header } from "../components";
+import { useStateContext } from "../contexts/ContextProvider";
+import { GROUP, HOME, LETTER } from "../constants/Role";
 
 const DashBoard = () => {
+    const { pageContent } = useStateContext();
   return (
     <div className="w-full h-fit bg-gray">
       <Header />
-      <Container></Container>
+      <Container>
+        {pageContent === HOME && (<h1>home</h1>)}
+        {pageContent === GROUP && (<h1>group</h1>)}
+        {pageContent === LETTER && (<h1>letter</h1>)}
+      </Container>
     </div>
   );
 };
