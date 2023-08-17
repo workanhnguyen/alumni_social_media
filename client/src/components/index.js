4e05c5a06991ba545e76ad1712ce13211468fcff
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-17 19:16:25 +0700

    index components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,5 @@ export { default as SignInForm } from './SignInForm';
 export { default as SignUpForm } from './SignUpForm';
 export { default as Copyright } from './Copyright';
 export { default as Header } from './Header';
+export { default as PostPanel } from './PostPanel';
+export { default as PostForm } from './PostForm';
