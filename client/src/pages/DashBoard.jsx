28bdb48c27fda8d824d1c9fd16df93a0607f1d09
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-17 11:54:09 +0700

    create dashboard

---
+++ client/src/pages/DashBoard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "@mui/material";
+import { Header } from "../components";
+
+const DashBoard = () => {
+  return (
+    <div className="w-full h-fit bg-gray">
+      <Header />
+      <Container></Container>
+    </div>
+  );
+};
+
+export default DashBoard;
