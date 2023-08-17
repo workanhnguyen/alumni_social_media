420facebd0a76cf578000b806cf5c80c5d5daa66
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-17 11:53:49 +0700

    index pages

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as ChooseRole } from './ChooseRole';
 export { default as AlumniLogin } from './alumni/AlumniLogin';
 export { default as AlumniRegister } from './alumni/AlumniRegister';
 export { default as LecturerLogin } from './lecturer/LecturerLogin';
+export { default as DashBoard } from './DashBoard';
