89bcf088c8bea23504bbf797e31f4c24e9230198
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-17 11:53:11 +0700

    index components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as SignInForm } from './SignInForm';
 export { default as SignUpForm } from './SignUpForm';
 export { default as Copyright } from './Copyright';
+export { default as Header } from './Header';
