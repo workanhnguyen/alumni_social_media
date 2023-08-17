5ea88adf6af7a6bd7345bc5a7649cdd7d63fa5ed
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-17 11:53:00 +0700

    index images

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,4 +1,7 @@
 import logoLong from './logo-long.png';
 import coverBg from './cover-bg.png';
+import logo1 from './logo1.png';
+import logo2 from './logo2.png';
+import blankAvatar from './blank-avatar.jpg';
 
-export { logoLong,coverBg };
+export { logoLong,coverBg, logo1, logo2, blankAvatar };
