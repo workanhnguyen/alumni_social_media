a81084380a07b4fd26576e5c614e611fab1a9e94
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-18 17:09:32 +0700

    create new page route

---
+++ client/src/pages/Letter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DefaultLayout } from '../layouts'
+
+const Letter = () => {
+  return (
+    <DefaultLayout>
+        Letters
+    </DefaultLayout>
+  )
+}
+
+export default Letter
