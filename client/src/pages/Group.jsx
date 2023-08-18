be110d7b8660f31f7d0b8946b1264ba5806411fd
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-18 17:09:24 +0700

    create new page route

---
+++ client/src/pages/Group.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DefaultLayout } from '../layouts'
+
+const Group = () => {
+  return (
+    <DefaultLayout>
+        Group
+    </DefaultLayout>
+  )
+}
+
+export default Group
