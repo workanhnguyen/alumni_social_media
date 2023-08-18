7df4ca1f51af05c7e5a7a7bdedf2dbf3fd8980b8
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-18 17:06:50 +0700

    index images

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,7 +1,19 @@
-import logoLong from './logo-long.png';
-import coverBg from './cover-bg.png';
-import logo1 from './logo1.png';
-import logo2 from './logo2.png';
-import blankAvatar from './blank-avatar.jpg';
+import logoLong from "./logo-long.png";
+import coverBg from "./cover-bg.png";
+import logo1 from "./logo1.png";
+import logo2 from "./logo2.png";
+import blankAvatar from "./blank-avatar.jpg";
+import actionHaha from "./action-haha.svg";
+import actionHeart from "./action-heart.svg";
+import actionLike from "./action-like.svg";
 
-export { logoLong,coverBg, logo1, logo2, blankAvatar };
+export {
+  logoLong,
+  coverBg,
+  logo1,
+  logo2,
+  blankAvatar,
+  actionHaha,
+  actionHeart,
+  actionLike,
+};
