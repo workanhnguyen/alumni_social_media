a6f87a47ce5d8cbbcf801edc15fe32ad08f2974d
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-18 17:09:14 +0700

    update ui

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 
 import { Container } from "@mui/material";
-import { Header, PostPanel } from "../components";
-import { useStateContext } from "../contexts/ContextProvider";
-import { GROUP, HOME, LETTER } from "../constants/Role";
+import { Post, PostPanel } from "../components";
+import { DefaultLayout } from "../layouts";
+import { postData } from "../data";
+
+const posts = postData;
 
 const DashBoard = () => {
-    const { pageContent } = useStateContext();
   return (
-    <div className="relative w-full h-fit bg-gray">
-      <Header />
-      <Container>
-        {pageContent === HOME && (<PostPanel className='my-6' />)}
-        {pageContent === GROUP && (<h1>group</h1>)}
-        {pageContent === LETTER && (<h1>letter</h1>)}
-      </Container>
-    </div>
+    <DefaultLayout>
+      <div className="w-full h-fit bg-gray">
+        <Container>
+          <PostPanel className="my-6" />
+          <div className="w-full flex flex-col items-center">
+            {posts.length > 0 &&
+              posts.map((post, index) => <Post key={index} data={post} />)}
+          </div>
+        </Container>
+      </div>
+    </DefaultLayout>
   );
 };
 
