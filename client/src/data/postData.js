9ca8fb5baf77da3c95635ffba9db3df06a78aa9a
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-18 17:08:44 +0700

    post sample data

---
+++ client/src/data/postData.js
@@ -0,0 +1,135 @@
+const posts = [
+    {
+      "id": 1,
+      "content": "Lorem ipsum dolor sit amet.",
+      "timestamp": "2023-08-06T10:00:00Z",
+      "is_lock": false,
+      "image": "https://res.cloudinary.com/dougpz2fu/image/upload/v1691309723/mobile-assets/blank_avatar_peyifv.jpg",
+      "user": {
+        "id": 101,
+        "first_name": "John",
+        "last_name": "Doe",
+        "avatar": "avatar_101.jpg"
+      }
+    },
+    {
+      "id": 2,
+      "content": "Consectetur adipiscing elit.",
+      "timestamp": "2023-08-06T11:30:00Z",
+      "is_lock": true,
+      "image": "https://res.cloudinary.com/dougpz2fu/image/upload/v1691309723/mobile-assets/blank_avatar_peyifv.jpg",
+      "user": {
+        "id": 102,
+        "first_name": "Jane",
+        "last_name": "Smith",
+        "avatar": "avatar_102.jpg"
+      }
+    },
+    {
+      "id": 3,
+      "content": "Sed do eiusmod tempor incididunt.",
+      "timestamp": "2023-08-06T12:45:00Z",
+      "is_lock": false,
+      "image": "https://res.cloudinary.com/dougpz2fu/image/upload/v1691309723/mobile-assets/blank_avatar_peyifv.jpg",
+      "user": {
+        "id": 103,
+        "first_name": "Alice",
+        "last_name": "Johnson",
+        "avatar": "avatar_103.jpg"
+      }
+    },
+    {
+      "id": 4,
+      "content": "Ut labore et dolore magna aliqua.",
+      "timestamp": "2023-08-06T14:15:00Z",
+      "is_lock": true,
+      "image": "https://res.cloudinary.com/dougpz2fu/image/upload/v1691309723/mobile-assets/blank_avatar_peyifv.jpg",
+      "user": {
+        "id": 104,
+        "first_name": "Bob",
+        "last_name": "Williams",
+        "avatar": "avatar_104.jpg"
+      }
+    },
+    {
+      "id": 5,
+      "content": "Excepteur sint occaecat cupidatat non proident.",
+      "timestamp": "2023-08-06T15:30:00Z",
+      "is_lock": false,
+      "image": "https://res.cloudinary.com/dougpz2fu/image/upload/v1691309723/mobile-assets/blank_avatar_peyifv.jpg",
+      "user": {
+        "id": 105,
+        "first_name": "Eve",
+        "last_name": "Brown",
+        "avatar": "avatar_105.jpg"
+      }
+    },
+    {
+      "id": 6,
+      "content": "Sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "timestamp": "2023-08-06T16:45:00Z",
+      "is_lock": true,
+      "image": "https://res.cloudinary.com/dougpz2fu/image/upload/v1691309723/mobile-assets/blank_avatar_peyifv.jpg",
+      "user": {
+        "id": 106,
+        "first_name": "Michael",
+        "last_name": "Davis",
+        "avatar": "avatar_106.jpg"
+      }
+    },
+    {
+      "id": 7,
+      "content": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "timestamp": "2023-08-06T18:00:00Z",
+      "is_lock": false,
+      "image": "https://res.cloudinary.com/dougpz2fu/image/upload/v1691309723/mobile-assets/blank_avatar_peyifv.jpg",
+      "user": {
+        "id": 107,
+        "first_name": "Emily",
+        "last_name": "Miller",
+        "avatar": "avatar_107.jpg"
+      }
+    },
+    {
+      "id": 8,
+      "content": "Cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "timestamp": "2023-08-06T19:15:00Z",
+      "is_lock": true,
+      "image": "https://res.cloudinary.com/dougpz2fu/image/upload/v1691309723/mobile-assets/blank_avatar_peyifv.jpg",
+      "user": {
+        "id": 108,
+        "first_name": "William",
+        "last_name": "Jones",
+        "avatar": "avatar_108.jpg"
+      }
+    },
+    {
+      "id": 9,
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "timestamp": "2023-08-06T20:30:00Z",
+      "is_lock": false,
+      "image": "https://res.cloudinary.com/dougpz2fu/image/upload/v1691309723/mobile-assets/blank_avatar_peyifv.jpg",
+      "user": {
+        "id": 109,
+        "first_name": "Olivia",
+        "last_name": "Wilson",
+        "avatar": "avatar_109.jpg"
+      }
+    },
+    {
+      "id": 10,
+      "content": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "timestamp": "2023-08-06T21:45:00Z",
+      "is_lock": true,
+      "image": "https://res.cloudinary.com/dougpz2fu/image/upload/v1691309723/mobile-assets/blank_avatar_peyifv.jpg",
+      "user": {
+        "id": 110,
+        "first_name": "Liam",
+        "last_name": "Martin",
+        "avatar": "avatar_110.jpg"
+      }
+    }
+];
+
+export default posts;
+  
