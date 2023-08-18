aa07a4bf166be709ab3c1d6970d733d374b50af5
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-18 14:23:15 +0700

    index components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,3 +4,5 @@ export { default as Copyright } from './Copyright';
 export { default as Header } from './Header';
 export { default as PostPanel } from './PostPanel';
 export { default as PostForm } from './PostForm';
+export { default as ImageUploader } from './ImageUploader';
+export { default as DivTextField } from './DivTextField';
