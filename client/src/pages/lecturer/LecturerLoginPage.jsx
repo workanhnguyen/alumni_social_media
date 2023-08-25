208cb0ce06dccca194d19036e66853f4c7c47e4f
workanhnguyen <anhnguyenvan0910@gmail.com>
2023-08-25 16:02:20 +0700

    update ui

--- client/src/pages/lecturer/LecturerLoginPage.jsx
+++ client/src/pages/lecturer/LecturerLoginPage.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
 
-import { ROLE_LECTURER } from '../../constants/role'
-import { SignInForm } from '../../components'
+import { ROLE_LECTURER } from "../../constants/role";
+import { PageException, SignInForm } from "../../components";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { ALREADY_LOGIN } from "../../constants/common";
 
 const LecturerLogin = () => {
+  const { token } = useStateContext();
   return (
-    <SignInForm role={ROLE_LECTURER} />
-  )
-}
+    <>
+      {token ? (
+        <PageException type={ALREADY_LOGIN} />
+      ) : (
+        <SignInForm role={ROLE_LECTURER} />
+      )}
+    </>
+  );
+};
 
-export default LecturerLogin
+export default LecturerLogin;
